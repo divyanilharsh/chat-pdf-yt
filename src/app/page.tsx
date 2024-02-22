33fc7edea817e7ac591c88dc90565301de09c329
divyanil harsh <divyanilharsh@divyanils-MacBook-Pro.local>
2024-02-22 18:49:39 +0530

    3rd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
-import { LogIn ,Link} from "lucide-react";
-import { STATIC_STATUS_PAGE_GET_INITIAL_PROPS_ERROR } from "next/dist/lib/constants";
+
 
 export default async function Home() {
-  const { userId } = await auth();
-  const isAuth = !!userId; // Added a semicolon at the end of the line
+  const { userId } = await auth() ;
+  const isAuth = !!userId ; // Added a semicolon at the end of the line
 
   return (
     <div className="w-screen min-h-screen" style={{ 
       background: 'linear-gradient(to right, rgb(236, 72, 153), rgb(239, 68, 68), rgb(234, 179, 8))',
-      position: 'relative' // Added a comma at the end of the line
+      position: 'relative', // Added a comma at the end of the line
     }}>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center"> {/* Changed "flex-colum" to "flex-col" */}
@@ -25,16 +26,18 @@ export default async function Home() {
           <p>
             Join millions of students, researchers, and professionals to instantly answer questions and understand research with AI. {/* Corrected spelling of "join", "professionals", and added missing periods */}
           </p>
-          <div className="w-full mt-4"> {/* Changed "dev" to "div" */}
+         <div className="w-full mt-2"> {/* Changed "dev" to "div" */}
             {isAuth ? (
               <h1>fileupload</h1>
             ) : (
+              
               <Link href="/sign-in">
-                <Button>Logging in to get started 
-                 
-                  <LogIn className="w-4 h-4 ml-2"/>
-                </Button> 
-              </Link>
+              <Button className="flex items-center justify-center w-4/5">
+                Logging in to get started 
+                <LogIn/>
+              </Button>
+            </Link>
+             
             )}
           </div>
         </div>

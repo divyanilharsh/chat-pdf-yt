8cb465ca07ac49c26a07ae652af5ab587d5e1046
divyanil harsh <divyanilharsh@divyanils-MacBook-Pro.local>
2024-02-24 19:11:59 +0530

    adding neon done

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,4 @@
-import{integer, pgTable, serial, text, timestamp, varchar}from 'drizzle-orm/pg-core'
+import{integer,  pgEnum, pgTable, serial, text, timestamp, varchar}from 'drizzle-orm/pg-core'
 
 export const userSystemEnum = pgEnum('user_system_enum',['system','user'])
 

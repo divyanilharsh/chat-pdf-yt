617f6f9ab027fe168e4417062a3619e4f7baa2b1
divyanilharsh <68350726+divyanilharsh@users.noreply.github.com>
2024-03-26 19:48:24 +0530

    rq done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,29 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import {} from "@clerk/nextjs"
 import { ClerkProvider } from "@clerk/nextjs";
-import Providers from "@/components/Provider";
-import {Toaster} from 'react-hot-toast'
+import Providers from "@/components/Providers";
+import { Toaster } from "react-hot-toast";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Create Next App",
-  description: "Chart pdf yt",
+  title: "ChatPDF YT",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
       <Providers>
-      <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <Toaster/>
-    </html>
-    
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+          <Toaster />
+        </html>
       </Providers>
-   
     </ClerkProvider>
   );
 }
---
+++ src/components/Providers.tsx
@@ -0,0 +1,16 @@
+'use client' // Add this line to mark it as a Client Component
+
+import React from 'react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+export default Providers

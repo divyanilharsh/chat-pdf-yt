82b091c706bbe05cbd39d751478800a9ee1e892b
divyanilharsh <68350726+divyanilharsh@users.noreply.github.com>
2024-05-09 13:29:59 +0530

    modified:   src/lib/pinecone.ts new file:   src/lib/s3-server.ts

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,5 +1,5 @@
 import {PineconeClient} from "@pinecone-database/pinecone";
-
+import { downloadFroms3 } from "./s3";
 
 let pinecone : PineconeClient | null = null ;
 
@@ -15,5 +15,6 @@ export const getPineconeClient = async() => {
     return pinecone
 }
 export async function loads3IntoPinecone(file_key: string) {
-    
+    console.log("downloading s3 into file system")
+    const file_name = await downloadFroms3(file_key)
 }
